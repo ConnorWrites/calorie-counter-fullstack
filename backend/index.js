55304476fc18502d93b44db13e4de391ea536760
Conrad Wilken <conradwilken@Conrads-MacBook-Pro.local>
2026-03-05 10:16:54 +0200

    Serve frontend from backend

--- backend/index.js
+++ backend/index.js
@@ -98,6 +98,17 @@ app.delete("/foods/:id", authenticateToken, (req, res) => {
   res.json({ success: true });
 });
 
+import path from "path";
+import express from "express";
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 /* -----------------------------
    Start server
 ------------------------------*/
