80fbe417071812c5dd061151dc853c72997f30a6
Conrad Wilken <conradwilken@Conrads-MacBook-Pro.local>
2026-03-05 10:08:42 +0200

    Updated API_URL to use Render URL

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import FoodList from "./components/FoodList";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
-const API_URL = "http://localhost:4000";
+const API_URL = "https://calorie-counter-fullstack.onrender.com";
 
 function App() {
   const [token, setToken] = useState(() => localStorage.getItem("token"));
