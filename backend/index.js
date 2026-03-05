ec7896ff31839151c73908f152bda3217999bde8
Conrad Wilken <conradwilken@Conrads-MacBook-Pro.local>
2026-03-05 13:10:13 +0200

    Deploy backend with frontend

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,13 @@ import db from "./database.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// ESM __dirname fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 const SECRET = process.env.JWT_SECRET || "your_jwt_secret_key";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // -----------------------------
 // Middleware
